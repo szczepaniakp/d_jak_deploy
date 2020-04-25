85e413329e82e57b97b614e7e388a46470383330
Patrycja Szczepaniak <szczepaniak7@gmail.com>
2020-04-25 17:31:45 +0200

    poprawki

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request, HTTPException, Response, Form, status
+from fastapi import FastAPI, Request, HTTPException, Response, Form, status, Query, Depends
 from fastapi.responses import RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 from typing import Dict
 # import redis
@@ -14,6 +15,7 @@ from base64 import b64encode
 from fastapi.templating import Jinja2Templates
 
 USERS = "users"
+security = HTTPBasic()
 
 app = FastAPI()
 # db = redis.Redis(host='localhost', charset="utf-8", decode_responses=True)
@@ -49,7 +51,7 @@ def hello_world():
 def hello_name(name: str):
     return HelloResp(message=f"hello {name}")
 
-@app.get('/welcome', response_model=HelloResp)
+@app.get('/hello', response_model=HelloResp)
 def welcome():
     return HelloResp(message="Hi there!")
 
@@ -57,31 +59,51 @@ def welcome():
 def load_login_form(request: Request):
     return templates.TemplateResponse("login.html", {"request": request}) 
 
-@app.post('/login')
-async def login(request: Request, *, login: str = Form(...), password: str = Form(...)):
+# @app.post('/login')
+async def login(request: Request, *, username: str = Form(...), password: str = Form(...)): #login: str = Query("login"), password: str = Query("pass")):#
     # print(username)
     # await request.form()
     # print(request.form())
     # username = request.form()["login"]
     # password = request.form()["pass"]
-    print(request.query_params)
-    login = request.query_params["login"] 
-    password = request.query_params["pass"] 
+    # print(request.query_params)
 
-    passes = b64encode(bytes(login + ':' + password, "utf-8"))
+    # login = request.query_params["login"] 
+    # password = "jdjd"
+    # print(request.headers)
+    # print(login)
+    passes = b64encode(bytes(username + ':' + password, "utf-8"))
 
     if passes not in hashed_passes:  #db.smembers(USERS):
         raise HTTPException(status_code=401, detail="Unauthorized") 
 
-    session_token = sha256(bytes(f"{login}{password}{app.secret_key}", 'utf-8')).hexdigest()
+    session_token = sha256(bytes(f"{username}{password}{app.secret_key}", 'utf-8')).hexdigest()
     #db.set(session_token, "session will expire in 5 minutes", ex=300)
     sessions.add(session_token)
-    response = RedirectResponse(url="/welcome", status_code=status.HTTP_302_FOUND) 
+    response = RedirectResponse(url="/hello", status_code=status.HTTP_302_FOUND) 
     response.set_cookie(key="session_token", value=session_token, expires=300)
     response.headers['Authorization'] = f"Basic {passes}" 
 
     return response
+    
+@app.post('/login')
+def read_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    password = credentials.password
 
+    passes = b64encode(bytes(username + ':' + password, "utf-8"))
+
+    if passes not in hashed_passes:  #db.smembers(USERS):
+        raise HTTPException(status_code=401, detail="Unauthorized") 
+
+    session_token = sha256(bytes(f"{username}{password}{app.secret_key}", 'utf-8')).hexdigest()
+    #db.set(session_token, "session will expire in 5 minutes", ex=300)
+    sessions.add(session_token)
+    response = RedirectResponse(url="/hello", status_code=status.HTTP_302_FOUND) 
+    response.set_cookie(key="session_token", value=session_token, expires=300)
+    response.headers['Authorization'] = f"Basic {passes}" 
+
+    return response
 
 @app.get('/method', response_model=MethodResp)
 @app.put('/method', response_model=MethodResp)

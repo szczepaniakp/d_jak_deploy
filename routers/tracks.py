70d93a5cbb10b7db6873ba60958fdf79466f9d33
Patrycja Szczepaniak <szczepaniak7@gmail.com>
2020-05-07 13:59:13 +0200

    w4 zad3

--- routers/tracks.py
+++ routers/tracks.py
@@ -27,7 +27,7 @@ def dict_factory(cursor, row):
 
 
 @router.get("/tracks")
-def get_tracks(request: Request, per_page: int = 10, page: int = 0):
+def tracks(request: Request, per_page: int = 10, page: int = 0):
     tracks = {}
     with sqlite3.connect(db_path) as connection:
         connection.row_factory = dict_factory
@@ -38,7 +38,7 @@ def get_tracks(request: Request, per_page: int = 10, page: int = 0):
     return JSONResponse(content=jsonable_encoder(tracks), status_code= status.HTTP_200_OK)
 
 @router.get("/tracks/composers/")
-def get_tracks_of_composer(request: Request, composer_name: str = ''):
+def tracks_of_composer(request: Request, composer_name: str = ''):
     data = []
 
     with sqlite3.connect(db_path) as connection:
@@ -56,21 +56,31 @@ def get_tracks_of_composer(request: Request, composer_name: str = ''):
     raise HTTPException(status_code=404, detail={"error": "Composer does not exist in database."})
 
 
-@router.post("/albums", response_model=AlbumResponse)
-async def add_album(request: Request, response: Response, album: Album):
+@router.post("/albums", response_model=AlbumResponse, status_code=status.HTTP_201_CREATED)
+async def album(request: Request, album: Album):
     with sqlite3.connect(db_path) as connection:
         connection.row_factory = dict_factory
         cursor = connection.cursor()
         artist_id = cursor.execute(f"SELECT ArtistId FROM artists WHERE ArtistId == { album.artist_id }").fetchone()
-        
+
         if not any(artist_id):
-            raise HTTPException(status_code=404, detail={"error": f"Artist with id={ album.artist_id } does not exists."})
+            raise HTTPException(status_code=404, detail={"error": f"Artist with id={ album.artist_id } does not exist."})
 
         cursor.execute(f"INSERT INTO albums (Title, ArtistId) VALUES ('{ album.title }', '{ album.artist_id }')")
         connection.commit()
-        # album_data = connection.cursor().execute(f"SELECT * FROM albums WHERE ArtistId == { album.artist_id }").fetchone()
-        # print()
+
     return AlbumResponse(AlbumId=cursor.lastrowid, Title=album.title, ArtistId=album.artist_id)
-    #JSONResponse(content=jsonable_encoder(tracks), status_code= status.HTTP_200_OK)
 
     
+@router.get("/albums/{album_id}", response_model=AlbumResponse, status_code=status.HTTP_200_OK)
+def albums(album_id: int):
+    album = []
+    
+    with sqlite3.connect(db_path) as connection:
+        connection.row_factory = dict_factory
+        cursor = connection.cursor()
+        album = cursor.execute(f"SELECT AlbumId, Title, ArtistId FROM albums WHERE AlbumId == {album_id}").fetchone()
+        if album is None:
+            raise HTTPException(status_code=404, detail={"error": f"Album with id={ album_id } does not exist."})
+
+    return AlbumResponse(**album)
